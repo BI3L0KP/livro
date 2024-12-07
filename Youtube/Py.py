e59f56ee2d0a491b08b2b2b53e6ffbcf53cd932e
Gabriel <gabrielkoehlerpupatto@gmail.com>
2024-12-07 11:36:36 -0400

    Add files via upload

---
+++ Youtube/Py.py
@@ -0,0 +1,22 @@
+"""
+frutas = ["maçã","banana","laranja"]
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+
+frutas = ["maçã","banana","uva"]
+frutas.append("goiaba")
+print (frutas)
+
+frutas.insert(0,"pera")
+print (frutas)
+
+frutas.remove ("pera")
+print(frutas)
+"""
+contador = 0
+while True:
+    print(contador)
+    contador += 1
+    if contador == 5:
+         break
